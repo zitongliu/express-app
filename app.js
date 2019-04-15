cb178cc91f0527191239cb0ba98031473e910491
zitongliu <zitongliu7@gmail.com>
2019-04-15 10:45:21 +1000

    express setup

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
-const http = require('http');
-
 const express = require('express');
 
 const app = express();
 
-const server = http.createServer(app);
+// Add a new middleware function
+// will be executed for every incoming request
+// next() allows the request to continue to the next middleware
+app.use((req, res, next) => {
+    console.log("In the middleware!");
+    next();
+}); 
 
-server.listen(3000);
+app.use((req, res, next) => {
+    console.log("In another middleware!");
+    res.send('<h1>Hello from Express!</h1>');
+}); 
+
+// calls http.createServer and then calls listen
+app.listen(3000);
